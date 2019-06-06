d1df960def4096a8fb80c98ca342a3be70ebc7ca
madeenawangjaem <madeena_20@hotmail.com>
2019-06-06 11:59:13 +0700

    asree

---
+++ ADT-API-CORE/ADT-API-CORE/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ADT_API_CORE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADT_API_CORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly _Context _context;
+
+        public ContactController(_Context context)
+        {
+            _context = context;
+        }
+        // GET: api/Province
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> Get()
+        {
+            return await _context.Contacts
+                .ToListAsync();
+        }
+
+        // POST: api/Contact{ }
+        [HttpPost]
+        public async Task<ActionResult<Contact>> Create([FromBody] Contact item)
+        {
+            return (Contact)await _context.Contacts
+                .Where(u => u.ContactID == item.ContactID)
+                .Where(u => u.OrganizationID == item.OrganizationID)
+                .Include(u => u.Organization)
+                .FirstOrDefaultAsync();
+
+        }
+
+    }
+}
--- ADT-API-CORE/ADT-API-CORE/Controllers/UserController.cs
+++ ADT-API-CORE/ADT-API-CORE/Controllers/UserController.cs
@@ -19,47 +19,26 @@ namespace ADT_API_CORE.Controllers
         {
             _context = context;
         }
-        // POST: api/User{username,password}
-        // POST: api/Province{name}
-        [HttpPost]
-        public ActionResult<User> Create([FromBody] User item)
-        {
-            return item;
-
-        }
+        
         // GET: api/User
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUser()
         {
-            return await _context.Users
+            return await _context.Users 
                 .Include(u => u.Organization)
                 .Include(u => u.Contact)
                 .ToListAsync();
         }
-
-        // GET: api/User/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/User
+        // POST: api/User/Username/Password
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<User>> Create([FromBody] User item)
         {
-        }
-
-        // PUT: api/User/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return (User)await _context.Users
+                .Where(u => u.UserName == item.UserName)
+                .Where(u => u.Password == item.Password)
+                .Include(u => u.Organization)
+                .Include(u => u.Contact)
+                .FirstOrDefaultAsync();
         }
     }
 }
